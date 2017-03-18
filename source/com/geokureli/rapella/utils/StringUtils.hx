3a4f8a95d14e960f093f0df30f5389489bef85e9
GeoKureli <Gkurelic@gmail.com>
2017-03-18 16:03:02 -0700

    add space trimmer

--- source/com/geokureli/rapella/utils/StringUtils.hx
+++ source/com/geokureli/rapella/utils/StringUtils.hx
@@ -4,6 +4,15 @@ package com.geokureli.rapella.utils;
  * ...
  * @author George
  */
-class StringUtils{
+class StringUtils {
     
+    static var _spaceTrimmer:EReg = ~/^\s*(.*?)\s*$/;
+    
+    static public function trimSpace(str:String):String {
+        
+        if(_spaceTrimmer.match(str))
+            return _spaceTrimmer.matched(1);
+        
+        return str;
+    }
 }
