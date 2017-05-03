5785a37c9c8b5a7d48f0f3c0c040066815049f72
GeoKureli <Gkurelic@gmail.com>
2017-05-03 10:39:40 -0700

    fix collision/platforming

--- source/com/geokureli/rapella/art/HeroWrapper.hx
+++ source/com/geokureli/rapella/art/HeroWrapper.hx
@@ -16,12 +16,24 @@ import openfl.geom.Rectangle;
 
 class HeroWrapper extends ScriptedWrapper {
     
-    static var WALK_SPEED:Point = new Point(10, 0);
-    static var RUN_SPEED :Point = new Point(20, 0);
+    // --- ZTHE CLOSEST YOU CAN GET TO A WALL5
+    static inline var BUFFER:Float = 0.1;
+    static inline var JUMP_HEIGHT:Float = -50;
+    static inline var JUMP_APEX_TIME:Float = 7;// --- FRAMES
+    static inline var JUMP_VELOCITY:Float = 2 * JUMP_HEIGHT / JUMP_APEX_TIME;
+    static inline var GRAVITY:Float = -2 * JUMP_HEIGHT / (JUMP_APEX_TIME * JUMP_APEX_TIME);
+    
+    static var WALK_SPEED:Point = new Point(10, 10);
+    static var RUN_SPEED :Point = new Point(20, 20);
     
     public var centerMassX(get, never):Float;
     public var centerMassY(get, never):Float;
     public var walls:Array<MovieClip>;
+    
+    // --- JUMP
+    var _jumpHeight  :Float;
+    var _jumpVelocity:Float;
+    var _gravity     :Float;
     
     // --- KEYS
     var _keyLeft :Bool;
@@ -41,11 +53,17 @@ class HeroWrapper extends ScriptedWrapper {
     
     var _anims:Map<String, AnimDef>;
     var _currentAnim:String;
+    var _v:Point;
+    var _onGround:Bool;
+    var _running:Bool;
+    var _canMove:Bool;
     
     public function new(mc:MovieClip) { super(mc); }
     
     override function setDefaults() {
         super.setDefaults();
+        
+        _v = new Point();
         
         isParent = true;
         _anims = [
@@ -83,37 +101,115 @@ class HeroWrapper extends ScriptedWrapper {
     
     override public function update():Void {
         
-        var vx:Float = (Key.checkAction(">") ? 1 : 0) - (Key.checkAction("<") ? 1 : 0);
-        var vy:Float = (Key.checkAction("v") ? 1 : 0) - (Key.checkAction("^") ? 1 : 0);
+        fixOverlaps();
         
-        if(vx != 0)
-            scaleX = vx * _originalScale.x;
-        
-        var run:Bool = Key.checkAction("run");
-        vx *= run ? RUN_SPEED.x : WALK_SPEED.x;
-        vy *= run ? RUN_SPEED.y : WALK_SPEED.y;
-        
-        if (vx != 0) {
+        if (_onGround) {
             
-            for (wall in walls) {
+            _v.x = ((Key.checkAction(">") ? 1 : 0) - (Key.checkAction("<") ? 1 : 0)) * _originalScale.x;
+            if(_v.x != 0)
+                scaleX = _v.x;
+            
+            _running = Key.checkAction("run");
+            _v.x *= _running ? RUN_SPEED.x : WALK_SPEED.x;
+            
+            if (Key.checkAction("jump")) {
+                _v.y += JUMP_VELOCITY * _originalScale.y;
+                _onGround = false;
+                play("jump");
+            }
+        } else
+            _v.y += GRAVITY / 2 * _originalScale.y;
+        
+        checkCollision();
+        
+        updateAnimation();
+        
+        x += _v.x;
+        y += _v.y;
+        
+        if (_onGround)
+            _v.y = 0;
+        else
+            _v.y += GRAVITY / 2 * _originalScale.y;
+    }
+    
+    function fixOverlaps():Void {
+        
+        var overlapX:Float;
+        var overlapY:Float;
+        
+        _onGround = false;
+        
+        for (wall in walls) {
+            
+            if (_top    < wall.y + wall.height
+            &&  _bottom > wall.y
+            &&  _left   < wall.x + wall.width
+            &&  _right  > wall.x) {
                 
-                if ((_left > wall.x + wall.width && _left + vx < wall.x + wall.width)
-                ||  (_right < wall.x && _right + vx > wall.x))
-                    vx = 0;
+                overlapX = wall.x + wall.width - _left;
+                if (overlapX > (wall.width + _bounds.width) / 2)
+                    overlapX -= wall.width + _bounds.width;
                 
-                if ((_top > wall.y + wall.height && _top + vy < wall.y + wall.height)
-                ||  (_bottom < wall.y && _bottom + vy > wall.y))
-                    vy = 0;
+                overlapY = wall.y + wall.height - _top;
+                if (overlapY > (wall.height + _bounds.height) / 2)
+                    overlapY -= wall.height + _bounds.height;
+                
+                // --- MOVE ALONG THE AXIS WITH THE LEAST OVERLAP
+                if (Math.abs(overlapX) < Math.abs(overlapY))
+                    x += overlapX;
+                else
+                    y += overlapY;
             }
             
-            if (run) play("run");
-            else     play("walk");
-            
-        } else
-            play("idle");
+            _onGround = _onGround
+                ||( _top    < wall.y + wall.height
+                &&  _bottom + BUFFER * 2 >= wall.y
+                &&  _left   < wall.x + wall.width
+                &&  _right  > wall.x);
+        }
+    }
+    
+    inline function checkCollision():Void {
         
-        x += vx;
-        y += vy;
+        for (wall in walls) {
+            
+            if (_top    < wall.y + wall.height
+            &&  _bottom > wall.y
+            &&  (  (_left  > wall.x + wall.width && _left  + _v.x < wall.x + wall.width)
+                || (_right < wall.x              && _right + _v.x > wall.x)))
+                _v.x = _v.x > 0 ? wall.x - _right - BUFFER : wall.x + wall.width - _left + BUFFER;
+            
+            if (!_onGround
+            && _left  + _v.x < wall.x + wall.width
+            &&  _right + _v.x > wall.x
+            &&  (  (_top    > wall.y + wall.height && _top    + _v.y < wall.y + wall.height)
+                || (_bottom < wall.y               && _bottom + _v.y > wall.y))) {
+                
+                _onGround = _v.y > 0;
+                _v.y = _v.y > 0 ? wall.y - _bottom - BUFFER : wall.y + wall.height - _top + BUFFER;
+            }
+        }
+    }
+    
+    inline function updateAnimation():Void {
+        
+        if (_onGround) {
+            
+            //if (_curentAnim == "jump") {
+                //
+                //vx = 0;
+                //play("land");
+                //
+            //} else 
+            if (_v.x == 0)     play("idle");
+            else if (_running) play("run");
+            else               play("walk");
+        } else {
+            
+            if (_currentAnim != "jump" && _currentAnim != "fall")
+                play("fall");
+        }
     }
     
     public function play(animKey:String, reset:Bool = false):Anim
@@ -133,10 +229,10 @@ class HeroWrapper extends ScriptedWrapper {
         return null;
     }
     
-    function get__left  ():Float { return target.x + _bounds.left  ; }
-    function get__right ():Float { return target.x + _bounds.right ; }
-    function get__top   ():Float { return target.y + _bounds.top   ; }
-    function get__bottom():Float { return target.y + _bounds.bottom; }
+    function get__left  ():Float { return x + _bounds.left  ; }
+    function get__right ():Float { return x + _bounds.right ; }
+    function get__top   ():Float { return y + _bounds.top   ; }
+    function get__bottom():Float { return y + _bounds.bottom; }
     
     public function get_centerMassX():Float { return _centerMass.x + x; }
     public function get_centerMassY():Float { return _centerMass.y + y; }
